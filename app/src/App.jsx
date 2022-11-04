6369569dae213640987c88d87a34acd9c0288113
kayeelai <117426751+kayeelai@users.noreply.github.com>
2022-11-04 16:33:19 +0800

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         </div>
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
 
